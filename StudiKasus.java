f89c8c6c35348393b1942c89561b16a9e2f470a4
zakariahaniya <zakalvha@gmail.com>
2024-12-02 11:48:23 +0700

    jobsheet 13

--- StudiKasus.java
+++ StudiKasus.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class StudiKasus {
-
     public static void main(String[] args) {
-        System.out.println(" ");
+        Scanner sc = new Scanner(System.in);
+        int menu;
+        System.out.println("=== PENCATATAN PRESTASI MAHASISWA ===");
+        System.out.println("1. Tambah Data Prestasi");
+        System.out.println("2. Tampilkan Semua Prestasi");
+        System.out.println("3. Analisis Prestasi Berdasarkan Jenis");
+        System.out.println("4. Keluar");
+        System.out.print("Pilih Menu : ");
+        menu = sc.nextInt();
     }
 }
